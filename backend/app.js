5791240bc18506bc0e8ec5ce1cef18b2eeac6568
MaxJS07 <maxjimenezsep@gmail.com>
2026-03-13 09:14:29 -0600

    CRUD completo funcional (Falta manejo de errores)

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ import productsRoute from "./src/routes/products.js"
 
 const app = express();
 
+//PARA ACEPTAR JSON DESDE POSTMAN:
+app.use(express.json())
+
 app.use("/api/products", productsRoute)
 
 export default app;
--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -9,3 +9,38 @@ productController.getProducts = async (req, res) => {
     const products = await productsModel.find()
     res.json(products)
 }
+
+//POST
+productController.insertProducts = async (req, res) =>{
+    //Solicitamos los campos
+    const {name, description, price, stock} = req.body;
+
+    const newProduct = new productsModel({name, description, price, stock})
+
+    await newProduct.save();
+
+    res.json({message: "Product saved"});
+}
+
+//ELIMINAR
+productController.deleteProducts = async (req, res) => {
+    await productsModel.findByIdAndDelete(req.params.id)
+    res.json({message: "Product deleted"})
+}
+
+//ACTUALIZAR
+productController.updateProducts = async (req, res) =>{
+    //1. Solicitamos valores nuevos
+    const {name, description, price, stock} = req.body;
+    await productsModel.findByIdAndUpdate(req.params.id, {
+        name,
+        description,
+        price,
+        stock
+    }, {new: true})
+
+    res.json({message: "Product updated"})
+
+};
+
+export default productController;
